c6f6b68142602b818eb0457a45695603f363ce70
goelakash <goelakash93@gmail.com>
2016-04-16 00:44:41 +0530

    RMSprop and 3-6-6-6-3 (x100) network

--- MonaNet.py
+++ MonaNet.py
@@ -1,73 +1,90 @@
+import sys
 import numpy as np
 import cv2
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD
-from random import randint
+from keras.optimizers import SGD, RMSprop
+from random import randint, shuffle
 
 image_file = cv2.imread("Mona Lisa.jpg")
 
-x1 = []
-x2 = []
-y1 = []
-y2 = []
-
-for i in xrange(image_file.shape[0]):
-    for j in xrange(image_file.shape[1]):
-        prob = randint(0,9)
-        if prob>0:
-            x1.append([i,j])
-            y1.append(image_file[i,j,:].astype('float32')/255.0)
-        else:
-            x2.append([i,j])
-            y2.append(image_file[i,j,:].astype('float32')/255.0)
-
-X_train = np.array(x1)
-Y_train = np.array(y1)
-X_test = np.array(x2)
-Y_test = np.array(y2)
-
-X = []
-X.append(X_train)
-X.append(X_test)
-
-Y = []
-Y.append(Y_train)
-Y.append(Y_test)
+h = image_file.shape[0]
+w = image_file.shape[1]
 
 model = Sequential()
+
 model.add(Dense(300, input_dim=2, init='uniform'))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+
+model.add(Dense(600, init='uniform'))
+model.add(Activation('relu'))
+
+model.add(Dense(600, init='uniform'))
+model.add(Activation('relu'))
+
+model.add(Dense(600, init='uniform'))
+model.add(Activation('relu'))
+
 model.add(Dense(300, init='uniform'))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(300, init='uniform'))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(300, init='uniform'))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(300, init='uniform'))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
+
 model.add(Dense(3, init='uniform'))
 model.add(Activation('linear'))
-model.add(Dropout(0.5))
 
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd)
+sgd = SGD(lr=0.1, momentum=0.94)
+model.compile(loss='mean_squared_error', optimizer=RMSprop())
 
-model.fit(X_train, Y_train,
-          nb_epoch=20,
-          batch_size=400,
+x1 = []
+y1 = []
+
+for i in xrange(h):
+    for j in xrange(w):
+        x1.append([i,j])
+        y1.append(image_file[i,j,:].astype('float32')/255.0)
+
+zip_1 = [i for i in zip(x1,y1)]
+
+nb_epochs = 500
+
+for e in xrange(nb_epochs):
+
+    
+    print "Epoch: ",e
+
+    shuffle(zip_1)
+
+    X_train = np.array([i for (i,j) in zip_1])
+    Y_train = np.array([j for (i,j) in zip_1])
+    
+    model.fit(X_train, Y_train,
+          nb_epoch=1,
+          batch_size=500,
           show_accuracy=True)
 
-score = model.evaluate(X_test, Y_test, batch_size=400)
 
-output_image = {}
 
-for i in X:
-    output_image[i] = model.predict(i)
+    X = []
+    X.extend(x1)
+    X = sorted(X)
 
-print output_image
+    predictions = model.predict(np.array(X))*255.0
+    for i in xrange(len(predictions)):
+        for j in xrange(3):
+            predictions[i][j] = min(predictions[i][j],255.0)
+    predictions = predictions.astype('uint8')
+    
+    output_image = []
+    index = 0
+
+    for i in xrange(h):
+        row = []
+
+        for j in xrange(w):
+            row.append(predictions[index])
+            index += 1
+
+        row = np.array(row)
+        output_image.append(row)
+
+    output_image = np.array(output_image)
+    cv2.imwrite('out_mona_3663(x100)_e'+str(e)+'.png',output_image)
